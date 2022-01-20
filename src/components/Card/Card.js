a87159154f6198b055b587277e94845df5c9da8c
Rustem <89071243+RustemShayakhmetov@users.noreply.github.com>
2022-01-20 14:56:32 +0500

    Update Card.js
    removing unnecessary const data

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,8 +6,8 @@ import "./Card.css"
 const Card = (listCompanies) => {
     const list = listCompanies.listCompanies
     const routeId = useParams()
-    const company = list.filter(item => item.id == routeId.id)
-    const data = company[0]
+    const company = list.filter(item => item.id == routeId.id)[0]
+
     return (
         <div>
             <h1>Просмотр карточки</h1>
@@ -15,14 +15,14 @@ const Card = (listCompanies) => {
                 <div className="card-open">
                     <h4>Карта компании</h4>
                     <hr />
-                    Название: {data.name} <br />
-                    ОГРН: {data.ogrn} <br />
-                    Дата регистрации: {data.registration} <br /> 
-                    Тип Компании: {data.type} <br />
-                    Активный: <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked={data.active} readOnly/> <br />
+                    Название: {company.name} <br />
+                    ОГРН: {company.ogrn} <br />
+                    Дата регистрации: {company.registration} <br /> 
+                    Тип Компании: {company.type} <br />
+                    Активный: <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked={company.active} readOnly/> <br />
                 </div>
                 <div className="buttons">
-                    <div className="btn edit"><Link to ={`/company/edit/${data.id}`} className="link">Редактировать</Link></div>
+                    <div className="btn edit"><Link to ={`/company/edit/${company.id}`} className="link">Редактировать</Link></div>
                     <div className="btn back"><Link to ="/" className="link">Закрыть</Link></div>
                 </div>
             </div>
